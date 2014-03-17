cb7827abd041d58327d8a8212a2a9ecf5c63b7b6
Peng Liu <myme5261314@gmail.com>
2014-03-17 13:27:25 +0800

    modify the genY.m to use only memory and save/load instead of memmapfile.

--- genY.m
+++ genY.m
@@ -3,6 +3,11 @@ function [ rawYmem ] = genY( params )
 %   This is the function to generate the cooresponding Y [m n] matrix, m is
 %   the num of data record, n is the num of output.
 
+if exist('e:/wuhan/RawY.mat', 'file')
+    load('e:/wuhan/RawY.mat');
+    return;
+end
+
 WindowSize = 64;
 Stride = 16;
 tF = @(l) floor((l - WindowSize)/Stride);
@@ -12,42 +17,46 @@ Xend = tF(params.numColumns);
 Yend = tF(params.numRows);
 pF = @(ind) (ind - 1)*Stride + 1;
 dataNum = Xend*Yend;
-if ~exist('F:/RawY.dat', 'file')
-    fId = fopen('F:/RawY.dat','w');
-    memMat = memmapfile('OSMMat.dat', 'format', {'double', [params.numRows params.numColumns], 'mat' });
-    rawYmem = memMat;
-    yMat = memMat.Data.mat;
-    count = 0;
-    per = 0.01;
-    for i = 1:Xend
-        for j = 1:Yend
-            count = count + 1;
-            if i==1 && j==1
-                tic;
-            end
-            if i==Xend && j == Yend
-                toc;
-            end
-            if count/dataNum >= per
-                toc;
-                fprintf('%d/%d\n', count, dataNum);
-                per = per + 0.01;
-                tic;
-            end
-            xStart = pF(i);
-            yStart = pF(j);
-            xStart = xStart + (WindowSize-Stride)/2;
-            yStart = yStart + (WindowSize-Stride)/2;
+
+rawYmem = zeros(dataNum, Stride^2);
+
+%     fId = fopen('F:/RawY.dat','w');
+%     memMat = memmapfile('OSMMat.dat', 'format', {'double', [params.numRows params.numColumns], 'mat' });
+%     rawYmem = memMat;
+%     yMat = memMat.Data.mat;
+count = 0;
+per = 0.01;
+for i = 1:Xend
+    for j = 1:Yend
+        count = count + 1;
+        if i==1 && j==1
+            tic;
+        end
+        if i==Xend && j == Yend
+            toc;
+        end
+        if count/dataNum >= per
+            toc;
+            fprintf('%d/%d\n', count, dataNum);
+            per = per + 0.01;
+            tic;
+        end
+        xStart = pF(i);
+        yStart = pF(j);
+        xStart = xStart + (WindowSize-Stride)/2;
+        yStart = yStart + (WindowSize-Stride)/2;
 %             matSize = [WindowSize WindowSize];
 %             tempMat = getImgMat(xStart, yStart, matSize, params);
 %             tempMat = permute(tempMat, [3 1 2]);
-            tempMat = yMat(yStart:yStart+Stride-1, xStart:xStart+Stride-1);
-            tempMat = tempMat(:);
-            fwrite(fId, tempMat, 'double');
-        end
+%             tempMat = yMat(yStart:yStart+Stride-1, xStart:xStart+Stride-1);
+        tempMat = params.osmMat(yStart:yStart+Stride-1, xStart:xStart+Stride-1);
+        tempMat = full(tempMat(:))';
+        rawYmem(count,:) = tempMat;
     end
-    fclose(fId);
 end
+assert(count==dataNum);
+save('E:/wuhan/RawY.mat', 'rawYmem', '-v7.3');
 
 end
+
 
