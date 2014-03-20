953bff92070aee33b3d836020d38c41714091fe0
Peng Liu <myme5261314@gmail.com>
2014-03-20 15:28:00 +0800

    Initial version of pcaReduce to reduce the dimension of X.

---
+++ pca_Reduce.m
@@ -0,0 +1,16 @@
+function [ reduceMat ] = pca_Reduce( rawMat, reduceDimension )
+%PCA_REDUCE Summary of this function goes here
+%   This is the function to perform the pca dimension reduce operation. The
+%   input is rawMat [m n](m data case, n feature num)
+%   reduceDimension is the num of feature dimension after pca reduce.
+%   The output is the data after pca reduce.
+rawMat = bsxfun(@minus, single(rawMat), mean(rawMat));
+rawMat = bsxfun(@rdivide, rawMat, max(rawMat) - min(rawMat));
+
+[coeff, score, latent] = pca(rawMat);
+per = sum(latent(1:reduceDimension,:))/sum(latent);
+fprintf('The remaining covariance is %f', per);
+reduceMat = rawMat * coeff(:,1:reduceDimension);
+
+end
+
