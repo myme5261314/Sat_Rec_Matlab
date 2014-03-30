69eabb9df1d1546bd68789cf721e0cd4341f3ca3
Peng Liu <myme5261314@gmail.com>
2014-03-31 04:20:16 +0800

    fix index exceeds matrix dimension issue.

--- partExec.m
+++ partExec.m
@@ -2,23 +2,22 @@ function result = partExec(mat, batchsize, exeFun)
 % This is the generalize version for part-wise calculate mean, std, cov and
 % some operation like that part result + part result / total num case.
 m = size(mat, 1);
-for i = 1:batchsize:m
-    if i==m-1
-        continue;
+idx = 1:batchsize:m;
+if idx(end) ~= m
+    idx = [idx m];
+end
+for i = 1:size(idx,2)-1
+    if i ~= size(idx,2)-1
+        temp = mat(idx(i):idx(i+1)-1, :);
+    else
+        temp = mat(idx(i):idx(i+1), :);
     end
-    temp = mat(i:i+batchsize-1, :);
     temp = exeFun(double(temp));
     if i==1
         result = temp;
     else
         result = result + temp;
     end
-end
-r = mod(m, batchsize);
-if r ~= 0
-    temp = mat(end-r+1:end, :);
-    temp = exeFun(double(temp));
-    result = result + temp;
 end
 % result = result/m;
 
