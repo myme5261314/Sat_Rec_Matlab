fe2a0d0696be613e8a4a0e870a11a465844428d0
Peng Liu <myme5261314@gmail.com>
2014-05-04 08:51:55 +0800

    Fix issues.

--- rbm_train.m
+++ rbm_train.m
@@ -231,9 +231,10 @@ function [idx] = findUselessData(data, WindowSize, StrideSize)
     newColIdx(1,:) = colIdx(1,ceil((1:StrideSize^2)/StrideSize));
     subIdx = zeros(1, 3*StrideSize^2);
     matSize = [WindowSize WindowSize 3];
-    subIdx(1,1:StrideSize^2) = sub2ind(matSize, newrowIdx, newColIdx, 1);
-    subIdx(1, StrideSize^2+1:2*StrideSize^2) = sub2ind(matSize, newrowIdx, newColIdx, 2);
-    subIdx(1, 2*StrideSize^2+1:3*StrideSize^2) = sub2ind(matSize, newrowIdx, newColIdx, 3);
+    channelIdx = ones(1, StrideSize^2);
+    subIdx(1,1:StrideSize^2) = sub2ind(matSize, newrowIdx, newColIdx, 1*channelIdx);
+    subIdx(1, StrideSize^2+1:2*StrideSize^2) = sub2ind(matSize, newrowIdx, newColIdx, 2*channelIdx);
+    subIdx(1, 2*StrideSize^2+1:3*StrideSize^2) = sub2ind(matSize, newrowIdx, newColIdx, 3*channelIdx);
     idx = ~any(data(:,subIdx),2);
 end
 
